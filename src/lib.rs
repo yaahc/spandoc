3d04392f16aa0af95d2bb1217bf3464fd5a121ab
Jane Lusby <jlusby@yaah.dev>
2020-01-03 10:49:19 -0800

    Working with more expr kinds

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate proc_macro;
 
+use matches::matches;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{fold::Fold, parse_quote, AttributeArgs, Block, ItemFn, Stmt};
@@ -36,16 +37,17 @@ impl Fold for SpanInstrumentedExpressions {
         let mut new_stmts = vec![];
 
         for mut stmt in stmts {
-            let doc_attr = match &mut stmt {
-                syn::Stmt::Local(local) => attr::doc(&mut local.attrs),
-                syn::Stmt::Item(_) => None,
-                syn::Stmt::Expr(_expr) => None,     // &expr.attrs,
-                syn::Stmt::Semi(_expr, ..) => None, // &expr.attrs,
-            };
-
-            let span = doc_attr.and_then(attr::as_span);
+            let attrs = attr::from_stmt(&mut stmt);
+            let span = attrs.and_then(attr::find_doc).and_then(attr::as_span);
 
             let stmts: Vec<Stmt> = match span {
+                Some(span) if matches!(stmt, Stmt::Expr(..)) => {
+                    parse_quote! {
+                        let __dummy_span = #span;
+                        let __dummy_span_guard = __dummy_span.enter();
+                        #stmt
+                    }
+                }
                 Some(span) => {
                     parse_quote! {
                         let __dummy_span = #span;
@@ -53,7 +55,6 @@ impl Fold for SpanInstrumentedExpressions {
                         #stmt
                         drop(__dummy_span_guard);
                         drop(__dummy_span);
-                        panic!("WOO");
                     }
                 }
                 _ => parse_quote! { #stmt },
@@ -69,9 +70,65 @@ impl Fold for SpanInstrumentedExpressions {
 
 mod attr {
     use quote::{quote, ToTokens};
-    use syn::{Attribute, LitStr, Meta};
+    use syn::{Attribute, Expr, LitStr, Meta, Stmt};
 
-    pub(crate) fn doc(attrs: &mut Vec<Attribute>) -> Option<Attribute> {
+    pub(crate) fn from_stmt(stmt: &mut Stmt) -> Option<&mut Vec<Attribute>> {
+        match stmt {
+            syn::Stmt::Local(local) => Some(&mut local.attrs),
+            syn::Stmt::Item(_) => None,
+            syn::Stmt::Expr(expr) => from_expr(expr),
+            syn::Stmt::Semi(expr, ..) => from_expr(expr),
+        }
+    }
+
+    fn from_expr(expr: &mut Expr) -> Option<&mut Vec<Attribute>> {
+        match expr {
+            Expr::Array(e) => Some(&mut e.attrs),
+            Expr::Assign(e) => Some(&mut e.attrs),
+            Expr::AssignOp(e) => Some(&mut e.attrs),
+            Expr::Async(e) => Some(&mut e.attrs),
+            Expr::Await(e) => Some(&mut e.attrs),
+            Expr::Binary(e) => Some(&mut e.attrs),
+            Expr::Block(e) => Some(&mut e.attrs),
+            Expr::Box(e) => Some(&mut e.attrs),
+            Expr::Break(e) => Some(&mut e.attrs),
+            Expr::Call(e) => Some(&mut e.attrs),
+            Expr::Cast(e) => Some(&mut e.attrs),
+            Expr::Closure(e) => Some(&mut e.attrs),
+            Expr::Continue(e) => Some(&mut e.attrs),
+            Expr::Field(e) => Some(&mut e.attrs),
+            Expr::ForLoop(e) => Some(&mut e.attrs),
+            Expr::Group(e) => Some(&mut e.attrs),
+            Expr::If(e) => Some(&mut e.attrs),
+            Expr::Index(e) => Some(&mut e.attrs),
+            Expr::Let(e) => Some(&mut e.attrs),
+            Expr::Lit(e) => Some(&mut e.attrs),
+            Expr::Loop(e) => Some(&mut e.attrs),
+            Expr::Macro(e) => Some(&mut e.attrs),
+            Expr::Match(e) => Some(&mut e.attrs),
+            Expr::MethodCall(e) => Some(&mut e.attrs),
+            Expr::Paren(e) => Some(&mut e.attrs),
+            Expr::Path(e) => Some(&mut e.attrs),
+            Expr::Range(e) => Some(&mut e.attrs),
+            Expr::Reference(e) => Some(&mut e.attrs),
+            Expr::Repeat(e) => Some(&mut e.attrs),
+            Expr::Return(e) => Some(&mut e.attrs),
+            Expr::Struct(e) => Some(&mut e.attrs),
+            Expr::Try(e) => Some(&mut e.attrs),
+            Expr::TryBlock(e) => Some(&mut e.attrs),
+            Expr::Tuple(e) => Some(&mut e.attrs),
+            Expr::Type(e) => Some(&mut e.attrs),
+            Expr::Unary(e) => Some(&mut e.attrs),
+            Expr::Unsafe(e) => Some(&mut e.attrs),
+            Expr::Verbatim(_) => None,
+            Expr::While(e) => Some(&mut e.attrs),
+            Expr::Yield(e) => Some(&mut e.attrs),
+            _ => None,
+            // some variants omitted
+        }
+    }
+
+    pub(crate) fn find_doc(attrs: &mut Vec<Attribute>) -> Option<Attribute> {
         let ind = attrs.iter().position(|attr| attr.path.is_ident("doc"));
 
         ind.map(|ind| attrs.remove(ind))
--- tests/happy.rs
+++ tests/happy.rs
@@ -1,12 +1,33 @@
 use spandoc::spandoc;
+use tracing::{info, instrument};
+use tracing_error::ErrorLayer;
+use tracing_subscriber::{fmt::FmtLayer, layer::Layer, registry::Registry, EnvFilter};
 
 #[spandoc]
 fn spanned() {
     /// Setting a to 1
-    let _a = 1;
+    let _a = eventful();
+}
+
+#[instrument]
+fn eventful() -> i32 {
+    /// so much to do, so little time
+    info!("doing the thing!");
+    42
 }
 
 #[test]
 fn happy() {
+    let filter = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+
+    let subscriber = ErrorLayer::default()
+        .and_then(FmtLayer::builder().with_target(false).finish())
+        .and_then(filter)
+        .with_subscriber(Registry::default());
+
+    tracing::subscriber::set_global_default(subscriber).expect("Could not set global default");
+
     spanned();
 }
